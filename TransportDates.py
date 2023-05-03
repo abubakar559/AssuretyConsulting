479fb8f0f68387c21a8965d7837fd483ba2a18bf
abubakar559 <abubakar559@outlook.com>
2023-05-03 16:11:45 +0500

    TransportDates

---
+++ TransportDates.py
@@ -0,0 +1,99 @@
+from tkinter import *
+from turtle import clear
+from selenium import webdriver
+from selenium.webdriver.common import keys
+from selenium.common.exceptions import NoSuchElementException
+import time
+import urllib.request
+import unittest
+from pathlib import Path
+import pickle
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+
+
+
+
+class Instagram(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get('http://aims.assuretyconsulting.com/AIMSPlus/Default.aspx')
+
+    def login(self):
+        pass
+
+
+    def test_login(self):
+
+        driver=self.driver
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_txtUserID"]').click()
+        driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_txtUserID"]').send_keys("administrator")
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_txtPassword"]').click()
+        driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_txtPassword"]').send_keys("abcd1234")
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_lbLogin"]').click()
+        time.sleep(10)
+        #login steps till now 
+
+        driver.find_element_by_xpath('//*[@id="ctl00_masterContentPlaceHolder_gdvwJobs_ctl02_lblhdrJobNameTitleAndIssue"]').click()
+        time.sleep(5)
+        #selecting first job 
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane3_header"]').click()
+        time.sleep(5) #left menu Modify     
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane3_content_lnkBtnManageCSM"]').click()
+        time.sleep(5)   #ManageCSM selection 
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_lbSelectAllContainers"]').click()
+        time.sleep(5) #SelectAllContainers
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_lbTransportManag"]').click()
+        time.sleep(5) #transport Selected 
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtScheduledInHomeDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtScheduledInHomeDate_CalendarExtender_today"]').click()
+        time.sleep(2) #Scheduled In-Home Date
+        
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtPostageStatementMailingDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtPostageStatementMailingDate_CalendarExtender_today"]').click()
+        time.sleep(2) #Postage Statement Mailing Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtScheduledShipDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtScheduledShipDate_CalendarExtender_today"]').click()
+        time.sleep(2)
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_btnSave"]').click()
+        time.sleep(2)
+
+
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane3_header"]').click()
+        time.sleep(5) #close modify drop down menu
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane5_header"]').click()
+        time.sleep(5) #open export menu drop down 
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane5_content_trPostalOneExport"]/td').click()
+        time.sleep(10) #selecting Postal One! 
+
+
+        
+
+
+    def tearDown(self):
+        time.sleep(5)
+        self.driver.close()
+
+    # def find_element(self):
+    #     try:
+    #         self.driver.find_element_by_class_name('_6CZji')
+    #         return True
+    #     except NoSuchElementException:
+    #         return False
+
+if __name__ == '__main__':
+    unittest.main()
