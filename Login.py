9d6ee7037e359a856986424ed0f2c0b960398774
abubakar559 <abubakar559@outlook.com>
2023-05-03 12:57:49 +0500

    CmsEditDatesUpdate

--- Login.py
+++ Login.py
@@ -37,36 +37,136 @@ class Instagram(unittest.TestCase):
         time.sleep(1)
         driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_lbLogin"]').click()
         time.sleep(10)
+        #login steps till now 
+
         driver.find_element_by_xpath('//*[@id="ctl00_masterContentPlaceHolder_gdvwJobs_ctl02_lblhdrJobNameTitleAndIssue"]').click()
         time.sleep(5)
+        #selecting first job 
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane3_header"]').click()
-        time.sleep(5)    
+        time.sleep(5) #left menu Modify     
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane3_content_lnkBtnManageCSM"]').click()
-        time.sleep(5)
+        time.sleep(5)   #ManageCSM selection 
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_lbSelectAllContainers"]').click()
-        time.sleep(5)
+        time.sleep(5) #SelectAllContainers
 
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_lbBatchEdit"]').click()
-        time.sleep(5)
+        time.sleep(5) #CSM Edit
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtbcsmPresortLabelingListEffectiveDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_cecsmPresortLabelingListEffectiveDate_today"]').click()
+        time.sleep(2) #Presort Labeling List Effective Date: todays date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtbcsmPresortCityStatePublicationDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_CalendarExtender3_today"]').click()
+        time.sleep(2) #Presort City State Publication Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtbPresortZoneChartMatrixPublicationDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_CalendarExtender5_today"]').click()
+        time.sleep(2) #Presort Zone Chart Matrix Publication Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtbLastUsedMailDirectionPublicationDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_CalendarExtender7_today"]').click()
+        time.sleep(2) #Last Used Mail Direction Publication Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtbLastUsedLabelingListEffectiveDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_cetbLastUsedLabelingListEffectiveDate_today"]').click()
+        time.sleep(2) #Last Used Labeling List Effective Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtbLastUsedCityStatePublicationDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_CalendarExtender4_today"]').click()
+        time.sleep(2) #Last Used City State Publication Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtbcsmLastUsedZoneChartMatrixPublicationDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_CalendarExtender6_today"]').click()
+        time.sleep(2) #Last Used Zone Chart Matrix Publication Date  
+
+
+        html = driver.find_element_by_tag_name('html').send_keys(Keys.PAGE_DOWN)
+        time.sleep(5) 
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtActualContainerShipDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtActualContainerShipDate_CalendarExtender_today"]').click()
+        time.sleep(2) #Actual Container Ship Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtScheduledPickUpDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtScheduledPickUpDate_CalendarExtender_today"]').click()
+        time.sleep(2) #Scheduled Pick Up Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtScheduledInHomeDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtScheduledInHomeDate_CalendarExtender_today"]').click()
+        time.sleep(2) #Scheduled In-Home Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtScheduledInductionDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtScheduledInductionDate_CalendarExtender_today"]').click()
+        time.sleep(2) #Scheduled Induction Start Date:
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtScheduledInductionEndDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtScheduledInductionEndDate_CalendarExtender_today"]').click()
+        time.sleep(2) #Scheduled Induction End Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtActualinductionDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtActualinductionDate_CalendarExtender_today"]').click()
+        time.sleep(2) #Actual Induction Date
+
+
+
         html = driver.find_element_by_tag_name('html').send_keys(Keys.PAGE_DOWN)
         time.sleep(5)
 
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtPostageStatementMailingDate"]').click()
-        time.sleep(2)
+        time.sleep(2) #Postage Statement Mailing Date to todays date
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtPostageStatementMailingDate_CalendarExtender_today"]').click()
-        time.sleep(2) 
+        time.sleep(2) #Postage Statement Mailing Date to todays date
+
+        html = driver.find_element_by_tag_name('html').send_keys(Keys.PAGE_DOWN)
+        time.sleep(5)
+
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtScheduledShipDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_txtScheduledShipDate_CalendarExtender_today"]').click()
+        time.sleep(2) #Scheduled Ship Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtReferenceableMailStartDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtReferenceableMailStartDateCalendarExtender_today"]').click()
+        time.sleep(2) #Referenceable Mail Start Date
+
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtReferenceableMailEndDate"]').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_ibtxtReferenceableMailEndDateCalendarExtender_today"]').click()
+        time.sleep(2) #Referenceable Mail End Date       
+        
+       
+
+
         html = driver.find_element_by_tag_name('html').send_keys(Keys.PAGE_DOWN)
         time.sleep(5)
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_masterContentPlaceHolder_btnSave"]').click()
-        time.sleep(5)
+        time.sleep(5) #click Save Button
         
         
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane3_header"]').click()
-        time.sleep(5)
+        time.sleep(5) #close modify drop down menu
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane5_header"]').click()
-        time.sleep(5)
+        time.sleep(5) #open export menu drop down 
         driver.find_element_by_xpath('//*[@id="ctl00_ctl00_masterContentPlaceHolder_LeftMenuJobDetail1_AccordionPane5_content_trPostalOneExport"]/td').click()
-        time.sleep(10)
+        time.sleep(10) #selecting Postal One! 
+
+
         #driver.find_element_by_xpath('').click()
         #driver.find_element_by_xpath('').click()
         #driver.find_element_by_xpath('').click()
